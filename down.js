9dc082f4310bea9c61cab9a87702dedfccd52c49
Gaurav Trikha <trikha.gaurav161999@gmail.com>
2020-05-21 22:15:54 +0530

    Downloading PDFs and showing name of each downloaded file and total downloaded files

--- down.js
+++ down.js
@@ -14,29 +14,33 @@ const csvWriter = createCsvWriter({
 	]	
 });
 let retdata = []; //array to store values extracted from the CSV file
-let low=0; //start of a batch of files to be downloaded
-let high=10; //end of a batch of files to be downloaded
+let low=0; //variable to store index of link for downloading 
+let high=1; //variable to store index of next link for downloading
 let complete=0; //variable to store the number of files downloaded 
 let chno=0; //variable that stores the chapter number
 let currSubject=''; //variable that holds the current Subject whose chapters are being downloaded
 let eng_part; //variable to store the English subpart of the Subject
 let hindi_part; //variable to store the Hindi subpart of the Subject
-async function dwnld (url1,fold,nm) { //function to download the files from the url1 and store them in the specified path	
+let c=0; //variable to select either English link or Hindi Link incase both are present in a row of the CSV file
+async function dwnld (url1,fold,nm) { //function to download the files from the url1 and store them in the specified path		
 	const path=Path.resolve(__dirname,`download1\\\\${fold}`,nm);
-	const response = await axios({ //initializing axios instance with the url, 'GET' method and stream value for respnseType 
+	const response = await axios({ //initializing axios instance with the url, 'GET' method and stream value for responseType 
 		method: 'GET',
 		url: url1,
 		responseType: 'stream',
 	})
-	response.data.pipe(fs.createWriteStream(path)); //piping the read-stream into the Node.js write-stream that points to a file at the specified path
+	response.data.pipe(fs.createWriteStream(path)); //piping the read-stream into the Node.js write-stream that points to a file at the specified path		
 	return new Promise((resolve,reject) => {
 		response.data.on('end',() => {
+			complete=complete+1; //incrementing the number of files downloaded
+			console.log(`Downloaded : ${fold}/${nm}`);
+			console.log(` ${complete} Chapters Downloads Finsihed`);
 	 		resolve()
 		}) 
-		response.data.on('error',err => {
-	 		reject(err)
+		response.data.on('error',(err)=>{
+			reject(err)
 		})
-	})
+	})	
 };
 
 async function storedata(){ //function for storing data in a CSV file
@@ -82,29 +86,42 @@ const startDownload= async () => { //function for downloading files
 			eng_part='';
 			hindi_part='';
 		}
-		if((!retdata[i].English_Link)&&(!retdata[i].Hindi_Link)) //Checking if the Subject has a subpart or not
+		if((!retdata[i].English_Link)&&(!retdata[i].Hindi_Link)) //Checking if the Subject has a subpart(unit) or not
 		{
-			chno=0;
+			chno=1;
 			eng_part=retdata[i].English_Chapter;
 			hindi_part=retdata[i].Hindi_Chapter;
 		}
-		if(retdata[i].English_Link) //Checking if the subject has a link for the English Chapters
+		if(retdata[i].English_Link&&retdata[i].Hindi_Link) //Checking if the subject has both the English as well as Hindi Links
+		{
+			if(c===0)
+			{
+				promises.push(dwnld(retdata[i].English_Link, retdata[i].Subject, eng_part+`-Chapter-${chno}(in-English).pdf`));	
+				high=high-1;
+				c=1;
+			}
+			else
+			{
+				promises.push(dwnld(retdata[i].Hindi_Link, retdata[i].Subject, hindi_part+`-Chapter-${chno}(in-Hindi).pdf`));
+				c=0;
+				chno=chno+1; //incrementing the chapter number
+			}
+		}
+		else if(retdata[i].English_Link) //Checking if the subject has only a link for the English Chapters
 		{
 			promises.push(dwnld(retdata[i].English_Link, retdata[i].Subject, eng_part+`-Chapter-${chno}(in-English).pdf`));
-			complete=complete+1; //incrementing the number of files downloaded
+			chno=chno+1; //incrementing the chapter number
 		}
-		if(retdata[i].Hindi_Link) //Checking if the subject has a link for the Hindi Chapters
+		else if(retdata[i].Hindi_Link) //Checking if the subject has only a link for the Hindi Chapters
 		{
 			promises.push(dwnld(retdata[i].Hindi_Link, retdata[i].Subject, hindi_part+`-Chapter-${chno}(in-Hindi).pdf`));
-			complete=complete+1; //incrementing the number of files downloaded
+			chno=chno+1; //incrementing the chapter number
 		}
-		chno=chno+1; //incrementing the chapter number
 	}
-	Promise.all(promises)
+	await Promise.all(promises)
 	.then(()=>{ //if all files for a batch are downloaded then displaying the message and calling the startDownload function to download the next batch
-		console.log(` ${complete} Chapters Downloads Finsihed`);
 		low=high;
-		high=high+10;
+		high=high+1;
 		if(high>retdata.length)
 		{
 			high=retdata.length;
@@ -116,9 +133,9 @@ const startDownload= async () => { //function for downloading files
 		
 	})
 	.catch((err)=>{ //if any of the file for a batch is not able to downloaded then displaying the message and calling the startDownload function to download the next batch
-		console.log("Error in downloading");
+		console.log(`Error in downloading ${err}`);
 		low=high;
-		high=high+10;
+		high=high+1;
 		if(high>retdata.length)
 		{
 			high=retdata.length;
@@ -128,8 +145,8 @@ const startDownload= async () => { //function for downloading files
 			startDownload();
 		}
 	});
+	
 }	
 getdata();
-console.log(retdata);
 //module.exports = getdata;
 
