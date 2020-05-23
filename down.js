dc26a4a71b808fa302a3a9d3b333234724f59dcc
Gaurav Trikha <trikha.gaurav161999@gmail.com>
2020-05-23 13:32:54 +0530

    Storing and Extracting data from a CSV file and downloading PDFs automatically(3)

--- down.js
+++ down.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const fs = require("fs");
 const Path = require("path");
 const getResults = require("./scraper");
-const createCsvWriter=require('csv-writer').createObjectCsvWriter;
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const csvWriter = createCsvWriter({
 	path: 'store2.csv', //path of the CSV file
 	header: [ //heeaders of the CSV file
@@ -14,14 +14,13 @@ const csvWriter = createCsvWriter({
 	]	
 });
 let retdata = []; //array to store values extracted from the CSV file
-let low=0; //variable to store index of link for downloading 
-let high=5; //variable to store index of next link for downloading
+let low=0; //variable to store starting index of batch of files for downloading 
+let high=5; //variable to store one more than last index of batch of files for downloading
 let complete=0; //variable to store the number of files downloaded 
 let chno=0; //variable that stores the chapter number
 let currSubject=''; //variable that holds the current Subject whose chapters are being downloaded
 let eng_part; //variable to store the English subpart of the Subject
 let hindi_part; //variable to store the Hindi subpart of the Subject
-let c=0; //variable to select either English link or Hindi Link incase both are present in a row of the CSV file
 async function dwnld (url1,fold,nm) { //function to download the files from the url1 and store them in the specified path		
 	const path=Path.resolve(__dirname,`download1\\\\${fold}`,nm);
 	const response = await axios({ //initializing axios instance with the url, 'GET' method and stream value for responseType 
@@ -72,18 +71,18 @@ const startDownload= async () => { //function for downloading files
 			eng_part=retdata[i].English_Chapter;
 			hindi_part=retdata[i].Hindi_Chapter;
 		}
-		if(retdata[i].English_Link) //Checking if the subject has only a link for the English Chapters
+		if(retdata[i].English_Link) //Checking if the subject has a link for the English Chapters
 		{
 			promises.push(dwnld(retdata[i].English_Link, retdata[i].Subject, eng_part+`-Chapter-${chno}(in-English).pdf`).catch((err)=>{console.log(err.message)}));
 		}
-		if(retdata[i].Hindi_Link) //Checking if the subject has only a link for the Hindi Chapters
+		if(retdata[i].Hindi_Link) //Checking if the subject has a link for the Hindi Chapters
 		{
 			promises.push(dwnld(retdata[i].Hindi_Link, retdata[i].Subject, hindi_part+`-Chapter-${chno}(in-Hindi).pdf`).catch((err)=>{console.log(err.message)}));
 		}
 		chno=chno+1; //incrementing the chapter number
 	}
 	await Promise.all(promises)
-	.then(()=>{ //if all files for a batch are downloaded then displaying the message and calling the startDownload function to download the next batch
+	.then(()=>{ //if all files for a batch are downloaded then calling the startDownload function to download the next batch
 		low=high;
 		high=high+5;
 		if(high>retdata.length)
@@ -95,7 +94,7 @@ const startDownload= async () => { //function for downloading files
 			startDownload();
 		}		
 	})
-	.catch((err)=>{ //if any of the file for a batch is not able to downloaded then displaying the message and calling the startDownload function to download the next batch
+	.catch((err)=>{ //if any of the file for a batch is not able to downloaded then calling the startDownload function to download the next batch
 		low=high;
 		high=high+5;
 		if(high>retdata.length)
@@ -111,7 +110,7 @@ const startDownload= async () => { //function for downloading files
 const getdata = async ()=> { //function to extract data from the CSV file
 	await storedata();
 	const csv = require('csv-parser');
-	fs.createReadStream('store.csv')
+	fs.createReadStream('store2.csv')
 	.pipe(csv())
 	.on('data', (data)=>{ //reading a row from the CSV file and writing it to the retdata array
 		retdata.push(data);
